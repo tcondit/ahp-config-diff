370a30eda39776b92e15ee03ab5ab5cd721863c3
Tim Condit <tcondit@gmail.com>
2014-12-30 21:19:04 -0800

    Experiment with sorting by element and id
    This project is currently in the "throw it at the wall and see if it
    sticks" phase. It's a mess. I'm trying to work out a routine for
    sorting the XML elements in a repeatable way.

--- configdifflxml.py
+++ configdifflxml.py
@@ -26,8 +26,39 @@ except ImportError:
         except ImportError:
           print("Failed to import ElementTree from any known place")
 
+from operator import itemgetter
 from pprint import pprint as pp
+import collections
+import re
 import sys
+
+
+# http://stackoverflow.com/questions/5967500/how-to-correctly-sort-a-string-with-a-number-inside
+#def atoi(text):
+#    return int(text) if text.isdigit() else text
+#
+#def natural_keys(text):
+#    '''
+#    alist.sort(key=natural_keys) sorts in human order
+#    http://nedbatchelder.com/blog/200712/human_sorting.html
+#    (See Toothy's implementation in the comments)
+#    '''
+#    return [ atoi(c) for c in re.split('(\d+)', text) ]
+#
+# Works okay for non-negative integers, but not negative ones like -2.
+#
+#alist=[
+#    "something1",
+#    "something12",
+#    "something17",
+#    "something2",
+#    "something-2",
+#    "something25",
+#    "something29"]
+#
+#alist.sort(key=natural_keys)
+#print(alist)
+#sys.exit(0)
 
 
 # ----------------------------------------------------------------------------
@@ -42,32 +73,6 @@ tree = etree.parse('b11f.xml')
 root = tree.getroot()
 # 'export'
 #print(root.tag)
-
-#print(type(tree))
-#print(type(root))
-#sys.exit(0)
-
-#print('root.tag:{}'.format(root.tag))
-#print('root.attrib:{}'.format(root.attrib))
-#
-# ----------------------------------------------------------------------------
-
-#print(etree.tostring(root))
-#
-#f = open('tree.xml', 'w')
-#f.write(etree.tostring(root))
-
-# ----------------------------------------------------------------------------
-#
-#parser2 = etree.XMLParser(ns_clean=True)
-#tree2 = etree.parse('builder10.xml')
-#
-#root2 = tree2.getroot()
-#
-#f2 = open('tree_nsclean.xml', 'w')
-#f2.write(etree.tostring(root))
-#
-# ----------------------------------------------------------------------------
 
 # tree.iter() returns a ...
 #for elem in tree.iter():
@@ -84,40 +89,33 @@ root = tree.getroot()
 #    print('  attrib:{}'.format(elem.attrib))
 #    print('  text:{}'.format(elem.text))
 
-#print(type(root.getchildren()))
-#
-#children = root.getchildren()
-#children.sort(key='tag')
-#
-#for child in children:
-#    print(child.tag)
-
 #  item = {}
 #  for elem in ia:
 #    item[elem.tag] = elem.text.strip()
 #  pp(item)
 
 #doc = etree.XML(data, etree.XMLParser(remove_blank_text=True))
-#for parent in doc.xpath('//*[./*]'):	# search for parent elements
-#    parent[:] = sorted(parent, key=lambda x: x.tag)
 for parent in root.xpath('//*[./*]'):	# search for parent elements
     parent[:] = sorted(parent, key=lambda x: x.tag)
 
-#print(etree.tostring(root, method='xml', pretty_print=True))
-#print(etree.tostring(root))
-#print(etree.tostring(doc, method='xml', pretty_print=True))
-#print(etree.tostring(doc))
-
-#for parent in root.xpath('//*[./*]'):	# search for parent elements
-#    parent[:] = sorted(parent, key=lambda x: x.attrib)
-
+# The child elements of the root are already ordered by tag. We can also get
+# the id of each child element. Combined, they make a unique identifier we can
+# use for sorting. But how to do a two-level sort, first by element name, and
+# then by id?
 
 # The child elements of the root are already ordered by tag. Now order each
 # element with the same tag (e.g., script-group, job-config, etc) by ID.
-children_dict = {}
+#children_dict = {}
+element_list = []
+odict = collections.OrderedDict()
+
 for elem in root.iterchildren():
-    tmp_tag = elem.tag + "_" + elem.attrib.get('id')
-    children_dict[tmp_tag] = elem
+    #tmp_tag = elem.tag + "_" + elem.attrib.get('id')
+    tmp_tag = elem.tag
+    tmp_attr = int(elem.attrib.get('id'))
+    element_list.append((tmp_tag, tmp_attr))
+#    children_dict[tmp_tag] = elem
+    odict[tmp_tag] = elem.attrib
     #children_dict[tmp_tag] = [elem.tag, elem.attrib, elem.attrib.get('id')]
 #    print('tmp_tag:{}'.format(tmp_tag))
 #    print('  element:{}'.format(elem.tag))
@@ -125,6 +123,24 @@ for elem in root.iterchildren():
 #    print('    attrib.id:{}'.format(elem.attrib.get('id')))
 #    print('    text:{}'.format(elem.text))
 
-print(sorted(children_dict.items()))
+#print(type(children_dict))
+#for k, v in children_dict.items():
+#    print(k, v)
+#print('#----------------------------------------------\n\n')
+#for k, v in odict.items():
+#    print(k, v)
+
+# inspect the list
+#print(element_list)
+
+# Doesn't sort numbers correctly (wah wah) but at least it's consistent. May
+# want to use the natural_keys sort above.
+for elem in sorted(element_list, key=itemgetter(0, 1)):
+    print(elem)
+
+#print(sorted(element_list))
+#alist.sort(key=natural_keys)
+
+#print(sorted(children_dict.items()))
 #print(sorted(children_dict.items(), key=lambda x: (x[1], x[0])))
 
