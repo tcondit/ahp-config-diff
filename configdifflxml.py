b6e9ca11f23769b3d6946466d5c700dd30ad96a0
Tim Condit <tcondit@gmail.com>
2014-12-31 08:04:33 -0800

    Decode binary text into ASCII
    ... and get pretty printing (caveat: given decent input) for free

--- configdifflxml.py
+++ configdifflxml.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+# TODO to be most useful, we'll need to format the XML before sorting and
+# printing it
+
 import collections
 import re
 import sys
@@ -9,40 +12,29 @@ from operator import itemgetter
 from pprint import pprint as pp
 
 
-## parse() returns an ElementTree object
 parser = etree.XMLParser()
-
-# ElementTree
 #tree = etree.parse('b10f.xml')
-tree = etree.parse('b11f.xml')
-
-# Element
+#tree = etree.parse('b11f.xml')
+tree = etree.parse('msb.xml')
 root = tree.getroot()
 
-# 'export'
-#print(root.tag)
-
-# sort by element tag (what I think of as name) first
+# Sort child elements by tag (what I think of as name) first
 for parent in root.xpath('//*[./*]'):
     parent[:] = sorted(parent, key=lambda x: x.tag)
-
-# The child elements of the root are already ordered by tag. We can also get
-# the id of each child element. Combined, they make a unique identifier we can
-# use for sorting. But how to do a two-level sort, first by element name, and
-# then by id?
 
 # The child elements of the root are already ordered by tag. Now order each
 # element with the same tag (e.g., script-group, job-config, etc) by ID.
 element_list = []
-odict = collections.OrderedDict()
 
 for elem in root.iterchildren():
     tmp_tag = elem.tag
     tmp_attr = int(elem.attrib.get('id'))
-    element_list.append((tmp_tag, tmp_attr, etree.tostring(elem)))
-    odict[tmp_tag] = elem.attrib
+    elem_str = etree.tostring(elem)
+    element_list.append((tmp_tag, tmp_attr, elem_str))
 
-# this is really ugly, but it may be progress
-for elem in sorted(element_list, key=itemgetter(0, 1)):
-    print(elem)
+# Get things in sorted order, then print only the last part of the tuple with
+# the XML string. key=itemgetter() does a two-level sort, first by element
+# name, and then by id. decode() produces raw ASCII from a binary string.
+for elem_str in sorted(element_list, key=itemgetter(0, 1)):
+    print(elem_str[2].decode('ascii'))
 
